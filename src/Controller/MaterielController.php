546db166396c462c00a16b318677e187ed752507
mmejjiou-boop <mmejjiou@gmail.com>
2026-06-30 11:24:21 +0200

    création du controller materiel ou il y a les routes pour la liste pour suprimer et pour créé

--- src/Controller/MaterielController.php
+++ src/Controller/MaterielController.php
@@ -37,23 +37,51 @@ class MaterielController extends AbstractController
     #[Route('', name: 'create', methods: ['POST'])]
     public function create(Request $request): JsonResponse
     {
+        $data = json_decode($request->getContent(), true);
+        if (!$data) return $this->json(['error' => 'Invalid JSON'], 400);
+        $materiel = new Materiel();
+        $materiel->setNom($data['nom'] ?? null);
+        $materiel->setCategorie($data['categorie'] ?? null);
+        $materiel->setReference($data['reference'] ?? null);
+        if (!empty($data['dateAchat'])) 
+            $materiel->setDateAchat(new \DateTime($data['dateAchat']));
+        $materiel->setDisponible($data['disponible'] ?? true);
+        $this->em->persist($materiel);
+        $this->em->flush();
 
-
+        return $this->json(['message' => 'le matériel a bien été créé avec un succès fulgurant'], 201);
 
     }
 
     #[Route('/{id}', name: 'update', methods: ['PUT'])]
     public function update(int $id, Request $request): JsonResponse
     {
-
-
-
+        // ici je récupere le matérielcen utilisant l id de l'urlet je le met dans $materiel
+        $materiel = $this->materielRepository->find($id);
+        // si le matériel n'existe pas je retourne une erreur 404
+        if (!$materiel) return $this->json(['error' => 'Materiel not found'], 404);
+        // là je récupere les données  de la requete et je les met dans $data
+        $data = json_decode($request->getContent(), true);
+        // si les données sont invalides je retourne une erreur 400
+        if (!$data) return $this->json(['error' => 'Invalid JSON'], 400);
+        // là je met à jour les données du (nom,categorie et reference et dateAchat et de disponible ) avec les données de la requete
+        if (array_key_exists('nom', $data)) {$materiel->setNom($data['nom']);}
+        if (array_key_exists('categorie', $data)) {$materiel->setCategorie($data['categorie']);}
+        if (array_key_exists('reference', $data)) {$materiel->setReference($data['reference']);}
+        if (array_key_exists('dateAchat', $data)) {$materiel->setDateAchat($data['dateAchat'] ? new \DateTime($data['dateAchat']) : null );}
+        if (array_key_exists('disponible', $data)) {$materiel->setDisponible($data['disponible']);}
+        $this->em->flush();
+        return $this->json(['message' => 'le matériel a bien été mi à jour']);
     }
 
     #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
     public function delete(int $id): JsonResponse
     {
-
+        $ùmateriel = $this->materielRepository->find($id);
+        if (!$materiel) return $this->json(['error' => 'Materielnontrouvé'], 404);
+        $this->em->remove($materiel);
+        $this->em->flush();
+        return $this->json(['message' => 'ce matériel a bien été supprimé']);
 
 
     }
